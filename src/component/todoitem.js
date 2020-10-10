8f452cae321f61af1bcfaa77097523986cfb0a39
ashrafahmed <40427830+ashrafahmedthabet@users.noreply.github.com>
2020-10-10 14:55:21 +0200

    Update todoitem.js

--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -2,7 +2,7 @@ import React from 'react';
 const TodoItems=(props)=>{
 return(
     <li  className=" list-group-item d-flex justify-content-between mb-3 border ">
-        <h6 onClick={props.handleComplete} className={props.complete?"text-capitalize active":"text-capitalize not-active"}>{props.task}</h6>
+        <h6 onClick={props.handleComplete} className={"cursor-pointer "+(props.complete?"text-capitalize active":"text-capitalize not-active")}>{props.task}</h6>
         <div>
             <span className="cursor-pointer mx-2 text-success"><i className=" fas fa-pen"onClick={props.handleEdit}></i></span>
             <span className="cursor-pointer mx-2 text-danger"onClick={props.handleDelete}><i className="fas fa-trash" ></i></span>
